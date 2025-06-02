1ab7b25bd5f592f143326c0c9a6cd27209d6064b
Rajiv <rajivpost@gmail.com>
2025-06-01 21:43:51 -0400

    main

--- app/registersite/page.tsx
+++ app/registersite/page.tsx
@@ -53,10 +53,6 @@ export default function RegisterSite() {
       const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "";
       const [httpvar, hostroot] = baseUrl.split("//");
       const redirectUrl = subdomainURL(siteId, "admin/managepage/PageCreate");
-      console.log("SiteId:", siteId);
-      console.log;
-      console.log("Redirect URL:", redirectUrl);
-      console.log("httpvar:", httpvar);
 
       setTimeout(() => {
         router.push(redirectUrl); // Redirect to the target page
@@ -122,10 +118,7 @@ export default function RegisterSite() {
                 Choose Your Site URL
               </Label>
               <div className="mt-2 w-full">
-                <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 w-full">
-                  <span className="flex select-none items-center pl-3 text-gray-500 sm:text-sm">
-                    reachpoint.site/
-                  </span>
+                <div className="flex rounded-md shadow-sm  w-full max-w-[200px] mx-auto">
                   <Input
                     type="text"
                     name="siteurl"
@@ -133,9 +126,12 @@ export default function RegisterSite() {
                     value={siteId}
                     onChange={(e) => setSiteId(e.target.value)}
                     autoComplete="siteurl"
-                    className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                    className="block flex-1 border-0 bg-transparent py-1.5 pl-1 pr-0 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6 text-right"
                     required
                   />
+                  <span className="inline-flex items-center justify-start pl-0 pr-3 text-gray-500 sm:text-sm">
+                    .reach.org
+                  </span>
                 </div>
                 <Button
                   type="button"
